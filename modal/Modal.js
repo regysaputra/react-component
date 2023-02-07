58bf39f3ebdda352b2f07ed9a755cf181f06a20c
regysaputra <regysaputra27@gmail.com>
2023-02-07 14:19:20 +0700

    Add createPortal

--- modal/Modal.js
+++ modal/Modal.js
@@ -1,81 +1,25 @@
-import React, {useState} from 'react'
+import React from 'react'
+import { createPortal } from 'react-dom'
 
-const SHOW_MODAL = "fixed inset-0 z-50 flex justify-center items-center";
-const HIDE_MODAL = "hidden";
+const OVERLAY_STYLES = 'fixed inset-0 z-50 bg-black/50'
+const MODAL_POS_STYLES = 'flex justify-center items-center h-full w-full'
 
-export default function Modal() {
-  const [isActive, setIsActive] = useState(false)
+export default function Modal ({ isOpen, close }) {
+  if (!isOpen) return null
 
-  const openModal = () => {
-    setIsActive(true)
-  }
-
-  const closeModal = () => {
-    setIsActive(false)
-  }
-
-  return (
-    <div>
-      <button
-        className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        type="button"
-        onClick={openModal}
-      >
-        Open Modal
-      </button>
-
-      {/* Main modal */}
+  return createPortal(
+    <>
       <div
-        style={{ backgroundColor: "rgba(0,0,0,0.5)" }}
-        className={isActive ? SHOW_MODAL : HIDE_MODAL}
+        className={OVERLAY_STYLES}
       >
-        {/* Modal content */}
-        <div className="relative bg-white rounded-lg shadow">
-          <button
-            type="button"
-            className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-            onClick={closeModal}
-          >
-            <svg
-              aria-hidden="true"
-              className="w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <span className="sr-only">Close modal</span>
-          </button>
-          <div className="px-6 py-6 lg:px-8">
-            <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
-              Tambah Category
-            </h3>
-            <form className="space-y-6">
-              <div>
-                <label
-                  htmlFor="category_name"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Category Name
-                </label>
-                <input
-                  type="text"
-                  name=""
-                  id=""
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  placeholder=""
-                  required
-                />
-              </div>
-            </form>
+        <div className={MODAL_POS_STYLES}>
+          {/* Modal content */}
+          <div className="relative w-1/4 bg-white rounded-lg shadow dark:bg-gray-700">
+            Modal
           </div>
         </div>
       </div>
-    </div>
+    </>,
+    document.getElementById('portal')
   )
 }
